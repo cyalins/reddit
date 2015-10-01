338cb38ac9c1834a2d268485d497ddfc3a49d032
cyalins <Marcus@Marcuss-Air.gateway>
2015-10-02 01:28:18 +1000

    Added basic list view

--- controllers.js
+++ controllers.js
@@ -2,24 +2,47 @@ angular.module('redditApp',[])
 	.controller('SubredditCtrl', function($scope, $http) {
 		// Initialisation of Materialize elements
 		$(".button-collapse").sideNav();
-		$scope.postlist = [
-			{"name": "George"},
-			{"name": "Peter"},
-			{"name": "Bob"},
-			{"name": "Ahoy"}
-			];
+
+		$scope.posts = undefined;
 		$scope.subreddit = "Android";
+		$scope.subredditinfo = [];
+
+		$http({
+			method: 'GET',
+			url: 'https://www.reddit.com/r/'+ $scope.subreddit + '/hot.json',
+			headers: {
+				// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
+				// "Access-Control-Allow-Origin": "*",
+				// "Accept": "application/x-www-form-urlencoded"
+			}
+		}).then(function(response){
+				$scope.postlist = response;
+				$scope.posts = $scope.postlist.data.data.children;
+				console.log(response);
+				console.log($scope.posts);
+		});
+		
 		$scope.submitSubreddit = function(subreddit) {
 			console.log(subreddit);
 			$http({
 				method: 'GET',
-				url: 'https://reddit.com/r/'+ subreddit + '/hot.json',
+				url: 'https://www.reddit.com/r/'+ subreddit + '/hot.json',
 				headers: {
-					// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4'
-					"Accept": "application/json"
+					// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
+					// "Access-Control-Allow-Origin": "*",
+					// "Accept": "application/x-www-form-urlencoded"
 				}
 			}).then(function(response){
-				$scope.postlist = response;
+				$scope.subredditinfo = response;
+				$scope.posts = $scope.subredditinfo.data.data.children;
+				console.log(response);
+				console.log($scope.posts);
 			});
+		};
+
+		$scope.openPost = function(postID) {
+			$scope.selectedPostID = postID;
+			
+
 		};
 	});
