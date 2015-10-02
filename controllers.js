f932ddbbd9dcdedb041cee177384e4ebd1716100
cyalins <Marcus@Marcuss-Air.gateway>
2015-10-02 20:34:51 +1000

    Added basic content display
    Post title, image, and self text are now shown.
    Added basic .crx manifest.
    Added overflow scrolling to post list.

--- controllers.js
+++ controllers.js
@@ -2,31 +2,19 @@ angular.module('redditApp',[])
 	.controller('SubredditCtrl', function($scope, $http) {
 		// Initialisation of Materialize elements
 		$(".button-collapse").sideNav();
+		$(document).ready(function(){
+			$('.materialboxed').materialbox();
+		});
 
 		$scope.posts = undefined;
-		$scope.subreddit = "Android";
+		$scope.subreddit = "all";
 		$scope.subredditinfo = [];
-
-		$http({
-			method: 'GET',
-			url: 'https://www.reddit.com/r/'+ $scope.subreddit + '/hot.json',
-			headers: {
-				// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
-				// "Access-Control-Allow-Origin": "*",
-				// "Accept": "application/x-www-form-urlencoded"
-			}
-		}).then(function(response){
-				$scope.postlist = response;
-				$scope.posts = $scope.postlist.data.data.children;
-				console.log(response);
-				console.log($scope.posts);
-		});
 		
 		$scope.submitSubreddit = function(subreddit) {
 			console.log(subreddit);
 			$http({
 				method: 'GET',
-				url: 'https://www.reddit.com/r/'+ subreddit + '/hot.json',
+				url: 'https://www.reddit.com/r/'+ $scope.subreddit + '/hot.json',
 				headers: {
 					// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
 					// "Access-Control-Allow-Origin": "*",
@@ -40,9 +28,24 @@ angular.module('redditApp',[])
 			});
 		};
 
+		$scope.submitSubreddit();
+
 		$scope.openPost = function(postID) {
 			$scope.selectedPostID = postID;
-			
-
+			console.log($scope.selectedPostID);
+			$http({
+				method: 'GET',
+				url: 'https://www.reddit.com/r/'+ $scope.subreddit + '/' + $scope.selectedPostID + '.json',
+				headers: {
+					// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
+					// "Access-Control-Allow-Origin": "*",
+					// "Accept": "application/x-www-form-urlencoded"
+				}
+			}).then(function(response){
+				$scope.selectedPostDetails = response;
+				console.log(response);
+				$scope.selectedPostOriginalPost = $scope.selectedPostDetails.data[0].data.children[0].data;
+				console.log($scope.selectedPostOriginalPost.selftext);
+			});
 		};
 	});
