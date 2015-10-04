992a00af2a8407b64d82cb2f587574b110501f35
cyalins <Marcus@Marcuss-Air.gateway>
2015-10-04 14:55:44 +1100

    Added moment.js and angular-moment; added basic timestamps to comments and posts; Added loading indicator; added favourites bar (currently uneditable)

--- controllers.js
+++ controllers.js
@@ -1,4 +1,4 @@
-angular.module('redditApp',[])
+angular.module('redditApp',['angularMoment'])
 	.controller('SubredditCtrl', function($scope, $http) {
 		// Initialisation of Materialize elements
 		$(".button-collapse").sideNav();
@@ -9,18 +9,22 @@ angular.module('redditApp',[])
 		$scope.posts = undefined;
 		$scope.subreddit = "all";
 		$scope.subredditinfo = [];
+		$scope.favourites = [{"name": "CitiesSkylines"}, {"name": "Anime"}, {"name": "LeagueOfLegends"}, {"name": "Android"}, {"name": "StardewValley"}, {"name": "NoMansSkyTheGame"}, {"name": "WorldNews"}, {"name": "AskReddit"}]
 		
 		$scope.submitSubreddit = function(subreddit) {
+			$scope.subreddit = subreddit;
 			console.log(subreddit);
+			$scope.loadingSelectedPost = true;
 			$http({
 				method: 'GET',
-				url: 'https://www.reddit.com/r/'+ $scope.subreddit + '/hot.json',
+				url: 'https://www.reddit.com/r/'+ subreddit + '/hot.json',
 				headers: {
 					// "Authorization": 'bearer yODuzy2H75ZV2Ll0noCuUwnNx-4',
 					// "Access-Control-Allow-Origin": "*",
 					// "Accept": "application/x-www-form-urlencoded"
 				}
 			}).then(function(response){
+				$scope.loadingSelectedPost = false;
 				$scope.subredditinfo = response;
 				$scope.posts = $scope.subredditinfo.data.data.children;
 				console.log(response);
@@ -28,10 +32,16 @@ angular.module('redditApp',[])
 			});
 		};
 
-		$scope.submitSubreddit();
+		$scope.submitSubreddit($scope.subreddit);
+		$scope.loadingSelectedPost = false;
+		$scope.hide = true;
+		$scope.hide2 = true;
+		$scope.hide3 = false;
+		$scope.hide4 = false;
 
 		$scope.openPost = function(postID) {
 			$scope.selectedPostID = postID;
+			$scope.loadingSelectedPost = true;
 			console.log($scope.selectedPostID);
 			$http({
 				method: 'GET',
@@ -42,6 +52,7 @@ angular.module('redditApp',[])
 					// "Accept": "application/x-www-form-urlencoded"
 				}
 			}).then(function(response){
+				$scope.loadingSelectedPost = false;
 				$scope.selectedPostDetails = response;
 				console.log(response);
 				$scope.selectedPostOriginalPost = $scope.selectedPostDetails.data[0].data.children[0].data;
